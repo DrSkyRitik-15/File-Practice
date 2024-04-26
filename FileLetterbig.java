4478cc7c0b2e327b7ec68e5758b0dd2370a7b9c7
DrSkyRitik-15 <ritiksinghkushwaha06@gmail.com>
2024-04-26 22:21:14 +0530

    .

---
+++ FileLetterbig.java
@@ -0,0 +1,30 @@
+
+import java.io.*;
+import java.util.Scanner;
+
+public class FileLetterbig {
+    public static void main(String[] args) {
+        try {
+            File file = new File("Red.txt");
+            
+            Scanner kola=new Scanner(file);
+            while(kola.hasNext()) 
+            {
+                StringBuilder as=new StringBuilder();
+                String lola=kola.next();
+                String O=lola.substring(0,1).toUpperCase()+lola.substring(1);
+                as.append(O).append(" ");
+                
+
+                System.out.print(as.toString());
+            }
+
+
+           
+
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("File not found or error reading the file.");
+        }
+    }
+}
