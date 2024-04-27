9d9cb75f7508689bfe9f23a80628b16f95805c8b
DrSkyRitik-15 <ritiksinghkushwaha06@gmail.com>
2024-04-27 08:53:49 +0530

    .

--- Read/ReverseFileContent.java
+++ Read/ReverseFileContent.java
@@ -48,8 +48,7 @@ public class ReverseFileContent {
     public static void main(String[] args) {
         try {
             File file = new File("Red.txt");
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line = reader.readLine();
+            
             // System.out.println(line);
             Scanner kola=new Scanner(file);
             while(kola.hasNextLine())          // line reverse
@@ -63,7 +62,7 @@ public class ReverseFileContent {
 
            
 
-            reader.close();
+            // reader.close();/
         } catch (IOException e) {
             System.out.println("File not found or error reading the file.");
         }
--- Read/reverse.java
+++ Read/reverse.java
@@ -6,8 +6,7 @@ public class reverse {
     public static void main(String[] args) {
         try {
             File file = new File("Red.txt");
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line = reader.readLine();
+            
             // System.out.println(line);
             Scanner kola=new Scanner(file);
             while(kola.hasNext())          // word reverse
@@ -21,7 +20,7 @@ public class reverse {
 
            
 
-            reader.close();
+            // reader.close();
         } catch (IOException e) {
             System.out.println("File not found or error reading the file.");
         }
